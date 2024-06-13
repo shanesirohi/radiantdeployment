6311c7c6c4a91fbc614cec599ededff23669e251
shanesirohi7 <shanesirohi@gmail.com>
2024-06-13 13:07:18 +0530

    preversion

---
+++ backend/models/userModel.js
@@ -0,0 +1,69 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      minLength: 6,
+      required: true,
+    },
+    profilePic: {
+      type: String,
+      default: "",
+    },
+    followers: {
+      type: [String],
+      default: [],
+    },
+    following: {
+      type: [String],
+      default: [],
+    },
+    bio: {
+      type: String,
+      default: "",
+    },
+    isFrozen: {
+      type: Boolean,
+      default: false,
+    },
+    school: {
+      type: String,
+      enum: ["SAJS", "AMITY", "DPS"], // Enumerate allowed options
+      required: true,
+    },
+    favoriteGenre: {
+      type: String,
+      default: "", // Default genre if needed
+    },
+    favoriteArtist: {
+      type: String,
+      default: "",
+    },
+    instagram: {
+      type: String,
+      default: "", // Add Instagram field
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
